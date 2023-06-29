3c966655854b8399ec648b1588c9a1fc4a6b1b22
MavileSeytoplaeva <mavile.seitoplaeva@gmail.com>
2023-06-29 21:16:50 +0300

    Доработка

--- src/test/java/pro/sky/skyprocalculator/CalculatorServiceParameterizedTest.java
+++ src/test/java/pro/sky/skyprocalculator/CalculatorServiceParameterizedTest.java
@@ -1,75 +1,46 @@
 package pro.sky.skyprocalculator;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CalculatorServiceParameterizedTest {
     CalculatorServiceImpl calculatorService = new CalculatorServiceImpl();
 
+    public static int[] calculatorProvider() {
+        return new int[]{6, 7,-8,9,0};
+    }
 
     @ParameterizedTest
-    @CsvSource({
-            "5,5",
-            "-5,-5",
-            "5,-5",
-            "5,0"
-
-    })
-    void plus(String a, String b) {
-        int num1 = Integer.parseInt(a);
-        int num2 = Integer.parseInt(b);
-        int actual = calculatorService.plus(num1, num2);
-        int expected = num1 + num2;
+    @MethodSource("calculatorProvider")
+    void plus(int a) {
+        int actual = calculatorService.plus(a,a);
+        int expected = a+a;
 
         Assertions.assertEquals(expected,actual);
     }
     @ParameterizedTest
-    @CsvSource({
-            "5,5",
-            "-5,-5",
-            "5,-5",
-            "5,0"
-
-    })
-    void minus(String a, String b) {
-        int num1 = Integer.parseInt(a);
-        int num2 = Integer.parseInt(b);
-        int actual = calculatorService.minus(num1, num2);
-        int expected = num1 - num2;
+    @MethodSource ("calculatorProvider")
+    void minus(int a) {
+        int actual = calculatorService.minus(a,a);
+        int expected = a - a;
 
         Assertions.assertEquals(expected,actual);
     }
     @ParameterizedTest
-    @CsvSource({
-            "5,5",
-            "-5,-5",
-            "5,-5",
-            "5,0"
+    @MethodSource ("calculatorProvider")
+    void multiply(int a) {
+        int actual = calculatorService.multiply(a, a);
+        int expected = a * a;
 
-    })
-    void multiply(String a, String b) {
-        int num1 = Integer.parseInt(a);
-        int num2 = Integer.parseInt(b);
-        int actual = calculatorService.multiply(num1, num2);
-        int expected = num1 * num2;
-
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
-    @ParameterizedTest
-    @CsvSource({
-            "5,5",
-            "-5,-5",
-            "5,-5",
-            "5,0"
 
-    })
-    void divide(String a, String b) {
-        int num1 = Integer.parseInt(a);
-        int num2 = Integer.parseInt(b);
-        int actual = calculatorService.divide(num1, num2);
-        int expected = num1 / num2;
+    @ParameterizedTest
+    @MethodSource("calculatorProvider")
+    void divide(int a) {
+        int actual = calculatorService.divide(a,a);
+        int expected = a / a;
 
         Assertions.assertEquals(expected,actual);
     }
