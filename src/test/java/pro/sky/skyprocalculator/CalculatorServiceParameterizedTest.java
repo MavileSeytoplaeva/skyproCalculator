bb64e5073a38ea4a872c68001192cee42e7715c2
Mavile <mavile.seitoplaeva@gmail.com>
2023-07-07 15:24:35 +0300

    добавила int b

--- src/test/java/pro/sky/skyprocalculator/CalculatorServiceParameterizedTest.java
+++ src/test/java/pro/sky/skyprocalculator/CalculatorServiceParameterizedTest.java
@@ -12,41 +12,41 @@ public class CalculatorServiceParameterizedTest {
     CalculatorServiceImpl calculatorService = new CalculatorServiceImpl();
 
     public static Stream<Arguments> calculatorProvider() {
-        return Stream.of(Arguments.of(-2,-4),
-                Arguments.of(0,0));
+        return Stream.of(Arguments.of(-2,4),
+                Arguments.of(7,0));
     }
 
 
     @ParameterizedTest
     @MethodSource("calculatorProvider")
-    void plus(int a) {
-        int actual = calculatorService.plus(a,a);
-        int expected = a+a;
+    void plus(int a, int b) {
+        int actual = calculatorService.plus(a,b);
+        int expected = a+b;
 
         Assertions.assertEquals(expected,actual);
     }
     @ParameterizedTest
     @MethodSource ("calculatorProvider")
-    void minus(int a) {
-        int actual = calculatorService.minus(a,a);
-        int expected = a - a;
+    void minus(int a, int b) {
+        int actual = calculatorService.minus(a,b);
+        int expected = a - b;
 
         Assertions.assertEquals(expected,actual);
     }
     @ParameterizedTest
     @MethodSource ("calculatorProvider")
-    void multiply(int a) {
-        int actual = calculatorService.multiply(a, a);
-        int expected = a * a;
+    void multiply(int a, int b) {
+        int actual = calculatorService.multiply(a, b);
+        int expected = a * b;
 
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
     @MethodSource("calculatorProvider")
-    void divide(int a) {
-        int actual = calculatorService.divide(a,a);
-        int expected = a / a;
+    void divide(int a, int b) {
+        int actual = calculatorService.divide(a,b);
+        int expected = a / b;
 
         Assertions.assertEquals(expected,actual);
     }
